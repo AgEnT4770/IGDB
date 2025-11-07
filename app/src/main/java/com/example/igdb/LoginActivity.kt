231e68fbaa2f08744391c27e1428b803b8ea073b
youssif-elsoufy <youssifelsoufy@gmail.com>
2025-11-07 15:30:20 +0200

    Add App logo and Modify Login & Signup designs

--- app/src/main/java/com/example/igdb/LoginActivity.kt
+++ app/src/main/java/com/example/igdb/LoginActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
@@ -36,6 +39,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -86,9 +90,18 @@ fun LoginDesign(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 32.dp)
-                .padding(top = 160.dp),
+                .padding(top = 20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
+            Image(
+                painter = painterResource(R.drawable.app_icn),
+                contentDescription = "IGDB Logo",
+                modifier = Modifier
+                    .padding(28.dp)
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.White.copy(alpha = 0.6f), CircleShape)
+            )
             Card(
 
                 shape = RoundedCornerShape(16.dp),
--- app/src/main/java/com/example/igdb/SignupActivity.kt
+++ app/src/main/java/com/example/igdb/SignupActivity.kt
@@ -6,14 +6,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -37,6 +42,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -87,9 +93,19 @@ fun SignupDesign(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 32.dp)
-                .padding(top = 160.dp),
+                .padding(top = 20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
+            Image(
+                painter = painterResource(R.drawable.app_icn),
+                contentDescription = "IGDB Logo",
+                modifier = Modifier
+                    .padding(28.dp)
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.White.copy(alpha = 0.6f), CircleShape)
+
+            )
             Card(
 
                 shape = RoundedCornerShape(16.dp),
@@ -163,7 +179,8 @@ fun SignupDesign(modifier: Modifier = Modifier) {
 
 @Composable
 fun SignupCredentials() {
-    var name by remember { mutableStateOf("") }
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
@@ -172,24 +189,48 @@ fun SignupCredentials() {
         Brush.linearGradient(listOf(Orange , Color.Magenta , White))
     }
 
-    OutlinedTextField(
-        value = name,
-        onValueChange = { name = it },
-        label = { Text("Full Name") },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Text),
-        textStyle = TextStyle(
-            brush = gradColors,
-            fontSize = 16.sp),
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = Color.Cyan,
-            unfocusedBorderColor = Color.LightGray,
-            focusedLabelColor = Color.Cyan,
-            unfocusedLabelColor = Color.LightGray
-        ),
-        singleLine = true,
-        modifier = Modifier.fillMaxWidth()
-    )
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ){
+        OutlinedTextField(
+            value = firstName,
+            onValueChange = { firstName = it },
+            label = { Text("First Name") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text),
+            textStyle = TextStyle(
+                brush = gradColors,
+                fontSize = 16.sp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Cyan,
+                unfocusedBorderColor = Color.LightGray,
+                focusedLabelColor = Color.Cyan,
+                unfocusedLabelColor = Color.LightGray
+            ),
+            singleLine = true,
+            modifier = Modifier.width(120.dp)
+        )
+        OutlinedTextField(
+            value = lastName,
+            onValueChange = { lastName = it },
+            label = { Text("Last Name") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text),
+            textStyle = TextStyle(
+                brush = gradColors,
+                fontSize = 16.sp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Cyan,
+                unfocusedBorderColor = Color.LightGray,
+                focusedLabelColor = Color.Cyan,
+                unfocusedLabelColor = Color.LightGray
+            ),
+            singleLine = true,
+            modifier = Modifier.width(120.dp)
+        )
+    }
 
     Spacer(modifier = Modifier.height(16.dp))
 
