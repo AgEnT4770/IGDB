555a6dbe52b875dd3a1d061929b0cb143b44549f
youssif-elsoufy <youssifelsoufy@gmail.com>
2025-11-08 00:20:31 +0200

    Sign Up Logic

--- app/src/main/java/com/example/igdb/SignupActivity.kt
+++ app/src/main/java/com/example/igdb/SignupActivity.kt
@@ -1,7 +1,9 @@
 package com.example.igdb
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -61,7 +63,9 @@ import com.example.igdb.ui.theme.IGDBTheme
 import com.example.igdb.ui.theme.LightBlue
 import com.example.igdb.ui.theme.Orange
 import com.example.igdb.ui.theme.White
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 lateinit var auth: FirebaseAuth
@@ -69,6 +73,7 @@ class SignupActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        auth = Firebase.auth
         setContent {
             IGDBTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -76,12 +81,35 @@ class SignupActivity : ComponentActivity() {
                 }
             }
         }
+
+    }
+
+
+}
+
+fun addUser(context: Context, email: String, pass: String) {
+    auth.createUserWithEmailAndPassword(email, pass)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                verifyEmail(context)
+            } else {
+                Toast.makeText(context, task.exception?.message ?: "Signup failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+}
+
+private fun verifyEmail(context: Context) {
+    val user = Firebase.auth.currentUser
+    user?.sendEmailVerification()?.addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+            Toast.makeText(context, "Check your inbox to verify your email", Toast.LENGTH_SHORT).show()
+            context.startActivity(Intent(context, LoginActivity::class.java))
+        }
     }
 }
 
 @Composable
 fun SignupDesign(modifier: Modifier = Modifier) {
-    val context = LocalContext.current
     Box(
         modifier = modifier
             .fillMaxSize(),
@@ -135,44 +163,6 @@ fun SignupDesign(modifier: Modifier = Modifier) {
 
                     SignupCredentials()
 
-                    Spacer(modifier = Modifier.height(32.dp))
-
-                    Button(
-                        onClick = {  },
-                        modifier = Modifier
-                            .width(108.dp)
-                            .height(50.dp)
-                            .align(Alignment.CenterHorizontally),
-                        shape = RoundedCornerShape(16.dp),
-                        colors = ButtonDefaults.buttonColors(containerColor = LightBlue)
-                    ) {
-                        Text("Sign Up", color = Color.White)
-                    }
-
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(
-                        text = "OR",
-                        color = Orange,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp,
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Button(
-                        onClick = {
-                            val intent = Intent(context, LoginActivity::class.java)
-                            context.startActivity(intent)
-                        },
-                        modifier = Modifier
-                            .width(108.dp)
-                            .height(50.dp)
-                            .align(Alignment.CenterHorizontally),
-                        shape = RoundedCornerShape(16.dp),
-                        colors = ButtonDefaults.buttonColors(containerColor = LightBlue)
-                    ) {
-                        Text("Login", color = Color.White)
-                    }
                 }
             }
         }
@@ -182,27 +172,111 @@ fun SignupDesign(modifier: Modifier = Modifier) {
 
 @Composable
 fun SignupCredentials() {
+    val context = LocalContext.current
     var firstName by remember { mutableStateOf("") }
     var lastName by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
     var passwordVisible by remember { mutableStateOf(false) }
+    var confirmPasswordVisible by remember { mutableStateOf(false) }
     val gradColors = remember {
         Brush.linearGradient(listOf(Orange , Color.Magenta , White))
     }
 
-    Row (
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-    ){
+    Column {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ){
+            OutlinedTextField(
+                value = firstName,
+                onValueChange = { firstName = it },
+                label = { Text("First Name") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text),
+                textStyle = TextStyle(
+                    brush = gradColors,
+                    fontSize = 16.sp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Cyan,
+                    unfocusedBorderColor = Color.LightGray,
+                    focusedLabelColor = Color.Cyan,
+                    unfocusedLabelColor = Color.LightGray
+                ),
+                singleLine = true,
+                modifier = Modifier.width(132.dp)
+            )
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = { lastName = it },
+                label = { Text("Last Name") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text),
+                textStyle = TextStyle(
+                    brush = gradColors,
+                    fontSize = 16.sp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Cyan,
+                    unfocusedBorderColor = Color.LightGray,
+                    focusedLabelColor = Color.Cyan,
+                    unfocusedLabelColor = Color.LightGray
+                ),
+                singleLine = true,
+                modifier = Modifier.width(132.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
-            value = firstName,
-            onValueChange = { firstName = it },
-            label = { Text("First Name") },
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text),
+                keyboardType = KeyboardType.Email),
+            textStyle = TextStyle(
+                brush = gradColors,
+                fontSize = 16.sp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Cyan,
+                unfocusedBorderColor = Color.LightGray,
+                focusedLabelColor = Color.Cyan,
+                unfocusedLabelColor = Color.LightGray
+            ),
+
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password),
+            visualTransformation = if (passwordVisible)
+                VisualTransformation.None
+            else
+                PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (passwordVisible)
+                    Icons.Default.Visibility
+                else
+                    Icons.Default.VisibilityOff
+
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(
+                        imageVector = image,
+                        contentDescription = if (passwordVisible) "Hide password"
+                        else "Show password",
+                        tint = if (passwordVisible) Color.White else Color.LightGray
+                    )
+                }
+            },
             textStyle = TextStyle(
                 brush = gradColors,
                 fontSize = 16.sp),
@@ -213,17 +287,39 @@ fun SignupCredentials() {
                 unfocusedLabelColor = Color.LightGray
             ),
             singleLine = true,
-            modifier = Modifier.width(120.dp)
+            modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = { Text("Last Name") },
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirm Password") },
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text),
+                keyboardType = KeyboardType.Password),
+            visualTransformation = if (confirmPasswordVisible)
+                VisualTransformation.None
+            else
+                PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (confirmPasswordVisible)
+                    Icons.Default.Visibility
+                else
+                    Icons.Default.VisibilityOff
+
+                IconButton(onClick = { confirmPasswordVisible = !confirmPasswordVisible }) {
+                    Icon(
+                        imageVector = image,
+                        contentDescription = if (confirmPasswordVisible) "Hide password"
+                        else "Show password",
+                        tint = if (confirmPasswordVisible) Color.White else Color.LightGray
+                    )
+                }
+            },
             textStyle = TextStyle(
                 brush = gradColors,
                 fontSize = 16.sp),
+
             colors = OutlinedTextFieldDefaults.colors(
                 focusedBorderColor = Color.Cyan,
                 unfocusedBorderColor = Color.LightGray,
@@ -231,111 +327,74 @@ fun SignupCredentials() {
                 unfocusedLabelColor = Color.LightGray
             ),
             singleLine = true,
-            modifier = Modifier.width(120.dp)
+            modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            onClick = {
+                when {
+                    email.isBlank() ->
+                        Toast.makeText(context, "Missing Email", Toast.LENGTH_SHORT).show()
+
+                    firstName.isBlank() ->
+                        Toast.makeText(context, "Missing First Name", Toast.LENGTH_SHORT).show()
+
+                    lastName.isBlank() ->
+                        Toast.makeText(context, "Missing Last Name", Toast.LENGTH_SHORT).show()
+
+                    password.isBlank() ->
+                        Toast.makeText(context, "Missing Password", Toast.LENGTH_SHORT).show()
+
+                    confirmPassword.isBlank() ->
+                        Toast.makeText(context, "Missing Password Confirmation", Toast.LENGTH_SHORT).show()
+
+                    password != confirmPassword ->
+                        Toast.makeText(context, "Passwords don't match", Toast.LENGTH_SHORT).show()
+
+                    password.length < 8 ->
+                        Toast.makeText(context, "Minimum password length is 8", Toast.LENGTH_SHORT).show()
+
+                    else -> {
+                        addUser(context, email, password)
+                    }
+                }
+            },
+            modifier = Modifier
+                .width(108.dp)
+                .height(50.dp)
+                .align(Alignment.CenterHorizontally),
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = LightBlue)
+        ) {
+            Text("Sign Up", color = Color.White)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "OR",
+            color = Orange,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                val intent = Intent(context, LoginActivity::class.java)
+                context.startActivity(intent)
+            },
+            modifier = Modifier
+                .width(108.dp)
+                .height(50.dp)
+                .align(Alignment.CenterHorizontally),
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = LightBlue)
+        ) {
+            Text("Login", color = Color.White)
+        }
     }
-
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        value = email,
-        onValueChange = { email = it },
-        label = { Text("Email") },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Email),
-        textStyle = TextStyle(
-            brush = gradColors,
-            fontSize = 16.sp),
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = Color.Cyan,
-            unfocusedBorderColor = Color.LightGray,
-            focusedLabelColor = Color.Cyan,
-            unfocusedLabelColor = Color.LightGray
-        ),
-
-        singleLine = true,
-        modifier = Modifier.fillMaxWidth()
-    )
-
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        value = password,
-        onValueChange = { password = it },
-        label = { Text("Password") },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Password),
-        visualTransformation = if (passwordVisible)
-            VisualTransformation.None
-        else
-            PasswordVisualTransformation(),
-        trailingIcon = {
-            val image = if (passwordVisible)
-                Icons.Default.Visibility
-            else
-                Icons.Default.VisibilityOff
-
-            IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                Icon(
-                    imageVector = image,
-                    contentDescription = if (passwordVisible) "Hide password"
-                    else "Show password",
-                    tint = if (passwordVisible) Color.White else Color.LightGray
-                )
-            }
-        },
-        textStyle = TextStyle(
-            brush = gradColors,
-            fontSize = 16.sp),
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = Color.Cyan,
-            unfocusedBorderColor = Color.LightGray,
-            focusedLabelColor = Color.Cyan,
-            unfocusedLabelColor = Color.LightGray
-        ),
-        singleLine = true,
-        modifier = Modifier.fillMaxWidth()
-    )
-    Spacer(modifier = Modifier.height(16.dp))
-
-    OutlinedTextField(
-        value = confirmPassword,
-        onValueChange = { confirmPassword = it },
-        label = { Text("Confirm Password") },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Password),
-        visualTransformation = if (passwordVisible)
-            VisualTransformation.None
-        else
-            PasswordVisualTransformation(),
-        trailingIcon = {
-            val image = if (passwordVisible)
-                Icons.Default.Visibility
-            else
-                Icons.Default.VisibilityOff
-
-            IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                Icon(
-                    imageVector = image,
-                    contentDescription = if (passwordVisible) "Hide password"
-                    else "Show password",
-                    tint = if (passwordVisible) Color.White else Color.LightGray
-                )
-            }
-        },
-        textStyle = TextStyle(
-            brush = gradColors,
-            fontSize = 16.sp),
-
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = Color.Cyan,
-            unfocusedBorderColor = Color.LightGray,
-            focusedLabelColor = Color.Cyan,
-            unfocusedLabelColor = Color.LightGray
-        ),
-        singleLine = true,
-        modifier = Modifier.fillMaxWidth()
-    )
 
 }
 
