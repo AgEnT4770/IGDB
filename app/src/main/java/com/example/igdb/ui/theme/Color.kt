f0bd307fb50a5eba909209f5f769c5c506709d82
Mohamed Ibrahim <mohamed.ibrahimturkey@gmail.com>
2025-11-30 16:48:04 +0200

    commit to pull...

--- app/src/main/java/com/example/igdb/ui/theme/Color.kt
+++ app/src/main/java/com/example/igdb/ui/theme/Color.kt
@@ -8,9 +8,9 @@ val LightNavyBlue = Color(0xFF1A1A2E)
 val Lavender = Color(0xFFA97BFF)
 val MutedLavender = Color(0xFF9A9A9E)
 
-val Purple80 = Color(0xFFA97BFF) // Primary
-val PurpleGrey80 = Color(0xFF9A9A9E) // Secondary
-val Pink80 = Color(0xFFEFB8C8) // Tertiary
+val Purple80 = Color(0xFFA97BFF)
+val PurpleGrey80 = Color(0xFF9A9A9E)
+val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
